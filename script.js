e227816e56e62d04caf777ff4999b56968a3329e
Sushil kumar <sushilindojin@gmail.com>
2025-08-24 11:19:00 +0530

    initial

---
+++ script.js
@@ -0,0 +1,21 @@
+debugger;
+let user = "amit";
+let fd = "rahul";
+let hg = "gungun";
+
+function sayhi() {
+  console.log("sds");
+}
+sayhi();
+function add() {
+  console.log("asdsd");
+}
+add();
+function sub() {
+  console.log("sdfsdf");
+}
+sub();
+function sayhi() {
+  console.log("sds");
+}
+sayhi();
